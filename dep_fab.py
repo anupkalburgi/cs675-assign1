8dce6435407c7d55fb6d27d1e64aec23d886b784
Anup Kalburgi(MAC) <anupkalburgi@gmail.com>
2014-09-23 10:46:32 -0400

    Removed the get_code and moved to refresh_code, which also includes tests to make sure that the old directory was removed and the new one was cloned from the remote repo

--- dep_fab.py
+++ dep_fab.py
@@ -13,36 +13,34 @@ def nodes():
                  'medusa-node5.vsnet.gmu.edu']
 
 
-
 def bootstrap():
     env.hosts = ['medusa-node1.vsnet.gmu.edu']
 
 
-def get_code():
-    with settings(warn_only=True):
-        if run("test -d %s" % code_dir).failed:
-            run("git clone  https://github.com/anupkalburgi/cs675-assign1.git %s" % code_dir)
-
 def refresh_code():
     with settings(warn_only=True):
         if not run("test -d %s" % code_dir).failed:
-            run('rm -rf %s' %code_dir)
+            run('rm -rf %s' % code_dir)
+        run("git clone  https://github.com/anupkalburgi/cs675-assign1.git %s" % code_dir)
 
 
 def bootstrap_deploy():
-    #run('python %s/dameon_start.py'%code_dir)
     #TODO--Before starting of with command check if the directoy exists
     with settings(warn_only=True):
-        #run('export PYRO_SERIALIZERS_ACCEPTED=serpent,json,marshal,pickle')
+        #run('export PYRO_SERIALIZERS_ACCEPTED=serpent,json,marshal,pickle') Right now i have it my \
+        # in my bash_profile, that will not work at scale
         # Right way of doing it is http://stackoverflow.com/questions/8313238/best-way-to-add-an-environment-variable-in-fabric
         run("pyro4-ns --host=%s >& /dev/null < /dev/null &" %env.hosts[0] )
         run('nohup python %s/bootstrap_node.py  >& /dev/null < /dev/null &' %code_dir)
 
+
 def node_deploy():
     run('nohup python %s/start_node.py >& /dev/null < /dev/null &' %code_dir)
 
+
 def kill():
     run("fuser -k 5150/tcp;sleep 1")
+
 
 def stop():
     run("fuser -k 9090/tcp;sleep 1")
