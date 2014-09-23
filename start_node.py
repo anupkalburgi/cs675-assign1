0ba925f1635a6e685d19ff6f852b59a02660ec18
Anup Kalburgi(MAC) <anupkalburgi@gmail.com>
2014-09-23 11:29:12 -0400

    Hardcoded the log location to check if that works

--- start_node.py
+++ start_node.py
@@ -4,7 +4,7 @@ from can_node import CAN_Node
 
 Pyro4.config.SERIALIZER = 'pickle'
 Pyro4.config.SERIALIZERS_ACCEPTED.add('pickle')
-Pyro4.config.PYRO_LOGFILE = 'logs/errors.log'
+Pyro4.config.PYRO_LOGFILE = '/home/akalburg/github/logs/errors.log'
 Pyro4.config.DETAILED_TRACEBACK = True
 '''
 Config:
