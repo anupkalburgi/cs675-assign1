868c0386b42b8b9131d4dc19232ca4161edbaa6e
Anup Kalburgi(MAC) <anupkalburgi@gmail.com>
2014-09-25 16:25:36 -0400

    Now the miss num

--- bootstrap_node.py
+++ bootstrap_node.py
@@ -26,7 +26,7 @@ def main():
     daemon = Pyro4.Daemon(host=get_host(), port=5150);
     node_uri = daemon.register(node)
     ns = Pyro4.locateNS(host='medusa-node1.vsnet.gmu.edu', port=9090)
-    ns.register("node."+str(id), node_uri)
+    ns.register("node."+str(1), node_uri)
     print "node."+str(node.id)
     print("Can Node running.")
     daemon.requestLoop()
