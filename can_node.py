71a17a20e53b39c11407e3e93a8843dbd2a81115
Anup Kalburgi(MAC) <anupkalburgi@gmail.com>
2014-09-27 21:20:59 -0400

    Putting in logger information so as to check what is causeing invalied URL

--- can_node.py
+++ can_node.py
@@ -140,6 +140,7 @@ class CAN_Node(object):
                 next_visit = min(to_visit, key=lambda n_node:n_node.id)
                 if next_visit:
                     visited.append(self)
+                    logger.info("View moving on to {0}".format(next_visit._id))
                     pyro_node = Pyro4.Proxy('PYRONAME:node.%s'%next_visit.id)
                     visited = pyro_node.view(visited,to_visit)
         return visited
